ad9e65bd7af96c2eebb86263a1541d945289c188
lion772 <williamlopes64@gmail.com>
2023-08-05 11:00:02 +0200

    install bootstrap, react-bootstrap, semantic-ui-css, semantic-ui-react, generate NavbarComponent tsx and css files, hook up navbar to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarComponent from "./components/NavbarComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavbarComponent></NavbarComponent>
+        </div>
+    );
 }
 
 export default App;
