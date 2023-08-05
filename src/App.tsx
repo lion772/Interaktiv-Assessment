b1ece1b5e91d184d652ba2269971caed9b424f58
lion772 <williamlopes64@gmail.com>
2023-08-05 14:27:40 +0200

    generate and set up HomepageDescription component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import "./App.css";
 import "semantic-ui-css/semantic.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComponent from "./components/NavbarComponent";
+import HomepageDescription from "./components/HomepageDescription";
 
 function App() {
     return (
         <div className="App">
-            <NavbarComponent></NavbarComponent>
+            <NavbarComponent>
+                <HomepageDescription />
+            </NavbarComponent>
         </div>
     );
 }
