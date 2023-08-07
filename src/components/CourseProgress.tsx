1b1470f2431385248f0b727851dd6e4a56393fb4
lion772 <williamlopes64@gmail.com>
2023-08-07 14:08:29 +0200

    add progress and missing properties, change LearningCourseDetail style

--- src/components/CourseProgress.tsx
+++ src/components/CourseProgress.tsx
@@ -1,9 +1,40 @@
 import { FC } from "react";
+import classes from "./CourseProgress.module.css";
 
-interface CourseProgressInt {}
+interface CourseProgressInt {
+    // progress is a value between 0 and 100 indicating the course progress
+    progress: number;
+    missing: number;
+}
 
-const CourseProgress: FC<CourseProgressInt> = () => {
-    return <></>;
+const CourseProgress: FC<CourseProgressInt> = ({ progress, missing }) => {
+    const circles: string[] = [];
+    const numberOfCircles: number = 5;
+    let content: any;
+
+    // Calculate the number of filled circles based on progress
+    const filledCircles = Math.ceil((progress / 100) * 5);
+
+    for (let i = 1; i <= numberOfCircles; i++) {
+        const circleClass = i <= filledCircles ? classes.filled : "";
+        circles.push(circleClass);
+    }
+
+    content = circles.map((circleClass: string, i) => (
+        <div key={i} className={`${classes.circle} ${circleClass}`}></div>
+    ));
+
+    console.log(missing);
+
+    return (
+        <div className={classes.courseProgress}>
+            <p>
+                {missing}min <br />
+                to be done by:
+            </p>
+            {content}
+        </div>
+    );
 };
 
 export default CourseProgress;
--- src/components/LearningCourseDetail.tsx
+++ src/components/LearningCourseDetail.tsx
@@ -9,11 +9,21 @@ interface LearningCourseDetailInt {
 
 const LearningCourseDetail: FC<LearningCourseDetailInt> = ({ course }) => {
     const { modules } = course;
+    let courseProgressShow: any;
     let content: any;
 
     if (modules.length > 1) {
         content = modules.map((module) => (
             <p key={module.id}>{module.topic}</p>
+        ));
+        courseProgressShow = modules.map(({ progress, missing }, index) => (
+            <div className={classes.progressContainer}>
+                <CourseProgress
+                    key={index}
+                    progress={progress}
+                    missing={missing}
+                />
+            </div>
         ));
     }
 
@@ -28,14 +38,12 @@ const LearningCourseDetail: FC<LearningCourseDetailInt> = ({ course }) => {
                         />
                     </div>
                     <div
-                        className="col-md-7 ms-3"
+                        className={"col-md-6 ms-1 " + classes.modulesContainer}
                         style={{ textAlign: "initial" }}
                     >
                         {content}
                     </div>
-                    <div className="col-md-4">
-                        <CourseProgress />
-                    </div>
+                    <div className={"col-md-5"}>{courseProgressShow}</div>
                 </div>
             </div>
         </div>
