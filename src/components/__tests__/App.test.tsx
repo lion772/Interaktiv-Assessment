6c45a8f43aed2c87688a7e73c88394d856ac2698
lion772 <williamlopes64@gmail.com>
2023-08-09 16:23:59 +0200

    install and setup enzyme and enzyme-adapter-react-16

---
+++ src/components/__tests__/App.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import App from "../../App";
+import { configureStore } from "@reduxjs/toolkit/dist/configureStore";
+import { useFetchCoursesQuery } from "../../store/api/coursesApi";
+
+describe("App Component", () => {
+    let wrapped: ShallowWrapper;
+    beforeEach(() => {});
+    test("shows HomePageDescription", () => {
+   
+    });
+});
