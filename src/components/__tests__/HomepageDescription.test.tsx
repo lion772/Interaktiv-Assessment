c0c8e43ace2c45966ab37eb4688f3a3f63714564
lion772 <williamlopes64@gmail.com>
2023-08-11 18:49:43 +0200

    add RTK/jest testing to HomepageDescription

--- src/components/__tests__/HomepageDescription.test.tsx
+++ src/components/__tests__/HomepageDescription.test.tsx
@@ -1,5 +1,8 @@
 import { ShallowWrapper, shallow } from "enzyme";
+import { render, screen } from "@testing-library/react";
 import HomepageDescription from "../HomepageDescription";
+import { Provider } from "react-redux";
+import { store } from "../../store";
 
 describe("HomepageDescription Component", () => {
     let wrapper: ShallowWrapper;
@@ -16,5 +19,31 @@ describe("HomepageDescription Component", () => {
         // Check if specific text elements are present
         expect(wrapper.find(".col-md-3 p").length).toBe(1);
         expect(wrapper.find(".col-md-9 p").length).toBe(1);
+    });
+});
+
+describe("HomepageDescription Component using Jest / RTK", () => {
+    test("renders the correct text content in the corresponding classes", () => {
+        // Render the component with Redux Provider and your store
+        render(
+            <Provider store={store}>
+                <HomepageDescription />
+            </Provider>
+        );
+
+        // Find the first div containing the text content
+        const colMd9P = screen.getByText("Home Page → e-Learning Courses");
+        expect(colMd9P).toBeInTheDocument();
+
+        // Find the second div containing the text content
+        const colMd3P = screen.getByText("admin");
+        expect(colMd3P).toBeInTheDocument();
+
+        // Check if specific text elements are present
+        const colMd9Ps = screen.getAllByText("Home Page → e-Learning Courses");
+        expect(colMd9Ps.length).toBe(1);
+
+        const colMd3Ps = screen.getAllByText("admin");
+        expect(colMd3Ps.length).toBe(1);
     });
 });
